f0d3a35bf40870040901af197076ea5998225493
Hackbright Student <info@hackbrightacademy.com>
2015-01-22 18:03:34 -0800

    forgot to save final changes before

--- melons.py
+++ melons.py
@@ -1,22 +1,5 @@
-# Melons
-#
-# fields are:
-#
-# species, color, is-imported (else is domestic), shape, seasons
-
-# melons = [
-#     ('Watermelon', 'green', False, 'natural', ['Fall', 'Summer']),
-#     ('Cantaloupe', 'tan', False, 'natural', ['Spring', 'Summer']),
-#     ('Casaba', 'green', True, 'natural', ['Spring', 'Summer', 'Fall', 'Winter']),
-#     ('Sharlyn', 'tan', True, 'natural', ['Summer']),
-#     ('Santa Claus', 'green', True, 'natural', ['Winter', 'Spring']),
-#     ('Christmas', 'green', False, 'natural', ['Winter']),
-#     ('Horned Melon', 'yellow', True, 'natural', ['Summer']),
-#     ('Xigua', 'black', True, 'square', ['Summer']),
-#     ('Ogen', 'tan', False, 'natural', ['Spring', 'Summer'])
-# ]
-
 class AbstractMelon(object):
+#create an exception if an instance of AbstractMelon is created
 
     def __init__(self):
         self.name = None
